aefa3d14b580bb8b48ae08b41cc7467ded9bd571
Alexander Wu <alexanderwu@Alexanders-MacBook-Pro.local>
2016-01-04 16:05:38 -0800

    simplified file system

---
+++ client/app.js
@@ -0,0 +1,45 @@
+angular.module('multiSnake', [])
+
+.factory('Game', function ($http) {
+  return {
+    getBoard: function(){
+      return $http.get('api/board').then(function(response){
+        return response.data;
+      });
+    },
+
+    connect: function(){
+      return $http.get('/api/connect').then(function(response){
+        return response.data;
+      });
+    },
+
+    setDirection: function(direction){
+      return $http.post('/api/direction', direction).then(function(response){
+        return response.data;
+      });
+    },
+
+    ready: function(state){
+      return $http.post('/api/ready', state).then(function(response){
+        return response.data;
+      });
+    },
+  };
+})
+
+.controller('mapController', function ($scope, Game) {
+  $scope.data = {};
+  $scope.data.ready = "this";
+  $scope.data.map = [];
+
+  Game.connect();
+
+  this.ready = function () {
+    this.data.ready = !this.data.ready;
+    Game.ready(this.data.ready);
+  }
+  Game.getBoard().then(function(data){
+    this.data.board = data;
+  });
+});
